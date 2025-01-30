fd3e20fa4bae5e68cd5aff275e0f547b9d725ee5
Dominick Deleon <dodeleon@chapman.edu>
2025-01-29 21:42:53 -0800

    Renamed Serialization.cs to Storage.cs Storage now adds to json files

--- Cpsc370Final/Serialization.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Cpsc370Final;
-
-public class Serialization
-{
-    
-}
---
+++ Cpsc370Final/Storage.cs
@@ -0,0 +1,37 @@
+﻿namespace Cpsc370Final;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class Storage
+{
+    private string jsonFileName;
+
+    public Storage(string fileName)
+    {
+        jsonFileName = fileName;
+    }
+        
+    public void AddObjectToJson<T>(T obj)
+    {
+        List<T> objects = ReadObjectsFromJson<T>();
+        objects.Add(obj);
+        
+        string json = JsonSerializer.Serialize(objects, new JsonSerializerOptions { WriteIndented = true });
+        
+        File.WriteAllText(jsonFileName, json);
+    }
+
+    public List<T> ReadObjectsFromJson<T>()
+    {
+        if (!File.Exists(jsonFileName))
+        {
+            return new List<T>();
+        }
+        
+        string json = File.ReadAllText(jsonFileName);
+        
+        return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+    }
+    
+}
