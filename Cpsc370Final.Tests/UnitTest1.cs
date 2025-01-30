80544759437b67caa4b81992d77cf8420f4c4a02
daniielahn <danielahnpar@gmail.com>
2025-01-29 20:54:36 -0800

    Spells Test

--- Cpsc370Final.Tests/UnitTest1.cs
+++ Cpsc370Final.Tests/UnitTest1.cs
@@ -23,4 +23,24 @@ public class UnitTest1
         
     }
     
+    [Fact]
+    public void SpellsTest()
+    { 
+        //Arrange
+        string ExpectedName = "Fire";
+        string ExpectedType = "Abjuration";
+        string ExpecetedDamage = "5";
+        
+        //Act
+        Spells spells = new Spells(ExpectedName, ExpectedType, ExpecetedDamage);
+        
+        //Assert
+        Assert.Equal(ExpectedName, spells.Name);
+        Assert.Equal(ExpectedType, spells.Type);
+        Assert.Equal(ExpecetedDamage, spells.Damage);
+    }
+    
+    
+    
+    
 }
