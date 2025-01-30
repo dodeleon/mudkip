8b9e6c0931b5777cc189225b3c31dc6f608fd90d
daniielahn <danielahnpar@gmail.com>
2025-01-29 20:52:08 -0800

    Spells Test and updated Spells.cs

--- Cpsc370Final.Tests/UnitTest1.cs
+++ Cpsc370Final.Tests/UnitTest1.cs
@@ -3,23 +3,21 @@ namespace Cpsc370Final.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void SpellsTest()
     {
+        //Arrange
+        string ExpectedName = "Fire";
+        string ExpectedType = "Abjuration";
+        string ExpecetedDamage = "5";
+        
+        //Act
+        Spells spells = new Spells(ExpectedName, ExpectedType, ExpecetedDamage);
+        
+        //Assert
+        Assert.Equal(ExpectedName, spells.Name);
+        Assert.Equal(ExpectedType, spells.Type);
+        Assert.Equal(ExpecetedDamage, spells.Damage);
     }
-
-    [Fact]
-    public void Constructor_ShouldInitializePropertiesCorrectly()
-    {
-        // Arrange
-        string expectedFeatName = "Test Feat"; 
-        string expectedFeatDescription = "This is a test feat description.";
-
-        // Act
-        Feat feat = new Feat(expectedFeatName, expectedFeatDescription);
-
-        // Assert
-        Assert.Equal(expectedFeatName, feat.FeatName);
-        Assert.Equal(expectedFeatDescription, feat.FeatDescription);
-    }
+    
     
 }
